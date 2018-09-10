db4eff8c9333dbf2b3d3417281c9dedea6f3422c
李茗浤 <vincent7697@gmail.com>
2018-09-10 18:29:04 +0800

    translate poc

--- test.js
+++ test.js
@@ -1,20 +1,23 @@
   // [START translate_list_codes]
   // Imports the Google Cloud client library
   const {Translate} = require('@google-cloud/translate');
+  var fs = require('fs');
 
   // Creates a client
   const translate = new Translate();
 
-  // Lists available translation language with their names in English (the default).
-  translate
-    .getLanguages()
-    .then(results => {
-      const languages = results[0];
+  console.log(fs.readFileSync('testword.txt'))
 
-      console.log('Languages:');
-      languages.forEach(language => console.log(language));
-    })
-    .catch(err => {
-      console.error('ERROR:', err);
-    });
-  // [END translate_list_codes]
+//   // Lists available translation language with their names in English (the default).
+//   translate
+//     .getLanguages()
+//     .then(results => {
+//       const languages = results[0];
+
+//       console.log('Languages:');
+//       languages.forEach(language => console.log(language));
+//     })
+//     .catch(err => {
+//       console.error('ERROR:', err);
+//     });
+//   // [END translate_list_codes]
