a191b0e1eb00f259be5e76681a32badc58a53e4a
李茗浤 <vincent7697@gmail.com>
2018-09-10 12:15:05 +0800

    translate poc

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-console.log('qwer')
+// Imports the Google Cloud client libraries
+const vision = require('@google-cloud/vision').v1;
+
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
+
+
+
+console.log(vision)
