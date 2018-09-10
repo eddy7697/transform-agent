eeedc554a9d989043e83b7d6f348768edec58e2f
李茗浤 <vincent7697@gmail.com>
2018-09-10 18:19:06 +0800

    translate poc

--- test.js
+++ test.js
@@ -1,59 +1,30 @@
-// Imports the Google Cloud client libraries
-const vision = require('@google-cloud/vision').v1;
+  // [START translate_detect_language]
+  // Imports the Google Cloud client library
+  const {Translate} = require('@google-cloud/translate');
 
-// Creates a client
-const client = new vision.ImageAnnotatorClient();
+  // Creates a client
+  const translate = new Translate();
 
-const bucketName = 'ctci_poc'
+  /**
+   * TODO(developer): Uncomment the following line before running the sample.
+   */
+  // const text = 'The text for which to detect language, e.g. Hello, world!';
 
-const fileName = 'testword.docx'
+  // Detects the language. "text" can be a string for detecting the language of
+  // a single piece of text, or an array of strings for detecting the languages
+  // of multiple texts.
+  translate
+    .detect(text)
+    .then(results => {
+      let detections = results[0];
+      detections = Array.isArray(detections) ? detections : [detections];
 
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-// Bucket where the file resides
-// const bucketName = 'my-bucket';
-// Path to PDF file within bucket
-// const fileName = 'path/to/document.pdf';
-
-const gcsSourceUri = `gs://${bucketName}/${fileName}`;
-const gcsDestinationUri = `gs://${bucketName}/${fileName}`;
-
-const inputConfig = {
-  // Supported mime_types are: 'application/pdf' and 'image/tiff'
-  mimeType: 'application/pdf',
-  gcsSource: {
-    uri: gcsSourceUri,
-  },
-};
-const outputConfig = {
-  gcsDestination: {
-    uri: gcsDestinationUri,
-  },
-};
-const features = [{type: 'DOCUMENT_TEXT_DETECTION'}];
-const request = {
-  requests: [
-    {
-      inputConfig: inputConfig,
-      features: features,
-      outputConfig: outputConfig,
-    },
-  ],
-};
-
-client
-  .asyncBatchAnnotateFiles(request)
-  .then(results => {
-    const operation = results[0];
-    // Get a Promise representation of the final result of the job
-    return operation.promise();
-  })
-  .then(filesResponse => {
-    let destinationUri =
-      filesResponse[0].responses[0].outputConfig.gcsDestination.uri;
-    console.log('Json saved to: ' + destinationUri);
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
+      console.log('Detections:');
+      detections.forEach(detection => {
+        console.log(`${detection.input} => ${detection.language}`);
+      });
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+  // [END translate_detect_language]
